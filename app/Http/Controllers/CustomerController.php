2cf81f22a4d2c5b756e16322b6f815b93f2f632f
B00165631 Alfred Ukpong <B00165631@mytudublin.ie>
2026-03-02 14:57:03 +0000

    Lab 1 Fully Complete

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Models\customer as Customer;
+
+class CustomerController extends Controller
+{
+    public function new()
+    {
+        return view('customers.new');
+    }
+
+    public function create(Request $request)
+    {
+        $customer = new Customer();
+
+        $customer->setFirstname($request->firstname);
+        $customer->setSurname($request->surname);
+
+        $customer->save();
+    }
+
+    public function edit($id)
+    {
+        $customer = Customer::find($id);
+        return view('customers.edit')->with('customer', $customer);
+    }
+
+    public function update(Request $request)
+    {
+        $customer = Customer::find($request->id);
+
+        $customer->setFirstname($request->firstname);
+        $customer->setSurname($request->surname);
+
+        $customer->save();
+    }
+}
